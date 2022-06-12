9f84e80ea47e5c3d8378669bf54909e50e34fed3
JermainLopez <97272097+JermainLopez@users.noreply.github.com>
2022-06-11 18:44:54 -0600

    Personal Assignment 2

---
+++ controllers/contacts.js
@@ -0,0 +1,27 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+//Function get all the database from mongodb and acces the colection in db
+const getAll = async(req, res, next) => {
+    const result = await mongodb.getDb().db().collection('contacts').find();
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+    });
+};
+//Get a single library fron db in mongodb
+const getSingle = async(req, res, next) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+        .getDb()
+        .db()
+        .collection('contacts')
+        .find({ _id: userId });
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    });
+};
+//Export the function por use in another file
+module.exports = { getAll, getSingle };
+
---
+++ db/connect.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { MongoClient } = require('mongodb');
+
+let _db;
+
+//Connect our mongodb
+const initDb = (callback) => {
+    if (_db) {
+        console.log('Db is already initialized!');
+        return callback(null, _db);
+    }
+    MongoClient.connect(process.env.MONGODB_URI)
+        .then((client) => {
+            _db = client;
+            callback(null, _db);
+        })
+        .catch((err) => {
+            callback(err);
+        });
+};
+
+//Send our client
+const getDb = () => {
+    if (!_db) {
+        throw Error('Db not initialized');
+    }
+    return _db;
+};
+
+module.exports = {
+    initDb,
+    getDb,
+};
