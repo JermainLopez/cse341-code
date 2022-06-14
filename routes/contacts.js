d509b1f2b93b4b0516375d15b7ded09dbc22d585
JermainLopez <97272097+JermainLopez@users.noreply.github.com>
2022-06-14 11:01:33 -0600

    Personal Assignment 2

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const router = express.Router();
-
 const contactsController = require('../controllers/contacts');
 
-router.get('/', contactsController.getAll);
 
-router.get('/:id', contactsController.getSingle);
+if (contactsController === require('../controllers/contacts')) {
+    const express = require('express');
+    const router = express.Router();
+    router.get('/', contactsController.getAll);
 
-module.exports = router;
+    router.get('/:id', contactsController.getSingle);
+
+    module.exports = router;
+} else {
+    const router = require('express').Router();
+
+    router.get('/', (req, res) => {
+        res.send('Starting')
+    });
+    module.exports = router;
+}
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,4 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/contacts', require('./contacts'))
-
 module.exports = router;
