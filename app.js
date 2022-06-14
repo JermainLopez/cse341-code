af49406bd943341eb99223c91f479b3a31249131
JermainLopez <97272097+JermainLopez@users.noreply.github.com>
2022-06-14 09:10:41 -0600

    update

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const path = require("path");
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
     .use(bodyParser.json())
+    .use(express.static(path.join(__dirname, '/')))
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
     })
     .use('/', require('./routes'));
+
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
